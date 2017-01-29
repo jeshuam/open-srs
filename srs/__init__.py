c9424a8172b70afb902e7ba90f0e695a9c7f2930
Jeshua Morrissey <jeshuamorrissey@gmail.com>
2017-01-29 13:30:47 -0800

    added basic website with stormpath auth

---
+++ srs/__init__.py
@@ -0,0 +1,16 @@
+from flask import Flask
+from flask_stormpath import StormpathManager
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'opensrs super secret'
+
+# TODO(jeshua): This stuff should not be here. Like, it really shouldn't. If
+# this ever becomes a real thing, please move these somewhere more secure.
+app.config['STORMPATH_API_KEY_ID'] = '646KV4LB30OGVAMHVEMFGK0N0'
+app.config[
+    'STORMPATH_API_KEY_SECRET'] = 'JSucwgxmRec03OV7D91+fLVg700JGAR8PvljlK2k3t4'
+app.config['STORMPATH_APPLICATION'] = 'Open SRS'
+
+stormpath_manager = StormpathManager(app)
+
+import srs.views
